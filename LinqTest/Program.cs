25ada92dd2734622f70feb146b248489b4da00bf
chajus1 <chajus1@googlemail.com>
2023-07-17 01:53:50 +0100

    Add project files.

---
+++ LinqTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i <= 99; i++)
+                numbers.Add(i);
+            IEnumerable<int> firstAndLastFive = numbers.
+        }
+    }
+}
