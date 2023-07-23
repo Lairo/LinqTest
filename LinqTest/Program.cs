df0da83bfbdc59e409721302f91f00e796ebac51
chajus1 <chajus1@googlemail.com>
2023-07-23 22:07:56 +0100

    mess

--- LinqTest/Program.cs
+++ LinqTest/Program.cs
@@ -1,15 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace LinqTest
 {
+
     class Program
     {
         static void Main()
         {
             List<int> numbers = new List<int>();
-            for (int i = 0; i <= 99; i++)
-                numbers.Add(i); 
-            IEnumerable<int> firstAndLastFive = numbers.Take(5).
+            for (int i = 1; i <= 99; i++)
+                numbers.Add(i);
+
+
+            //IEnumerable<int> firstFive = numbers.Take(5);
+            //IEnumerable<int> lastFive = numbers.TakeLast(5);
+
+            IEnumerable<int> lastFive = numbers.Skip(numbers.Count() - 5);
+            IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(lastFive);
+
+
+            foreach (int i in firstAndLastFive)
+            {
+                Console.Write($"{i} ");
+            }
         }
     }
 }
